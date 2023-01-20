8eca2bf9325bef33f7bc7ca5b36ca40d5a3d3d05
trsalondra <trsalondra@gmail.com>
2023-01-19 18:03:50 -0600

    added delete button

--- src/components/TodoDetails.js
+++ src/components/TodoDetails.js
@@ -1,11 +1,26 @@
-export default function TodoDetails({ todo }) {
+import { useTodosContext } from "../hooks/useTodosContext"
 
-        
+export default function TodoDetails({ todo }) {
+    const { dispatch } = useTodosContext()
+
+    const handleClick = async () => {
+        const response = await fetch('http://localhost:3001/todos/' + todo._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({ type: 'DELETE_TODO', payload: json })
+        }
+    }
+
     return (
         <div>
             <h6>{todo.title}</h6>
-            {/* <h5>{todo.dueDate}</h5>
-            <h4>{todo.description}</h4> */}
+            <h5>{todo.dueDate}</h5>
+            <h4>{todo.description}</h4>
+
+            <span onClick={handleClick}>delete</span>
             <hr />
         </div>
     )
--- src/context/TodosContext.js
+++ src/context/TodosContext.js
@@ -13,6 +13,10 @@ export const todosReducer = (state, action) => {
             return {
                 todos: [action.payload, ...state.todos]
             }
+        case 'DELETE_TODO':
+            return {
+                todos: state.todos.filter((w) => w._id !== action.payload._id)
+            }
         default:
             return state
     }
